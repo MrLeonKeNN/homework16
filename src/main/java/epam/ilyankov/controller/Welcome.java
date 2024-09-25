e113d330f148b769fe2edfc09b7b01561fb007f5
Vyacheslav Ilyankov <61803026+MrLeonKeNN@users.noreply.github.com>
2024-09-25 16:56:10 +0300

    123

--- src/main/java/epam/ilyankov/controller/Welcome.java
+++ src/main/java/epam/ilyankov/controller/Welcome.java
@@ -22,7 +22,7 @@ public class Welcome extends HttpServlet {
                 "<body>\n" +
                 "    <form method=\"POST\" action = \"Cell\">\n" +
                 "        <div style=\"display: flex; flex-direction: column;width: 15%; margin: auto;\">\n" +
-                "            <p>Welcome to Online Shop123</p>\n" +
+                "            <p>Welcome to Online shopping</p>\n" +
                 "            <input type=\"text\" placeholder=\"enter you name\" name=\"name\" id=\"\">\n" +
                 "<p><input type=\"checkbox\" name=\"option\" >I agree with the terms of service</p>" +
                 "            <button>Enter</button>\n" +
